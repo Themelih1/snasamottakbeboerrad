2ca43497dbae20a86704c3c5fd0cd674b2b3f6fc
Themelih1 <melih.yilmaz904@gmail.com>
2025-06-20 17:04:03 +0200

    entrypoint.sh dosyasında gerekli değişiklikler yapıldı

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,6 @@ mkdir -p logs
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Creating admin user if not exists..."
-python manage.py shell -c "
-from django.contrib.auth.models import User;
-username = 'egemen';
-email = 'tuglacihukuk@gmail.com';
-password = 'Egemen4004!';
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print('Admin user created.')
-else:
-    print('Admin user already exists.')
-"
-
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
