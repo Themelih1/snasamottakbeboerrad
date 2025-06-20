75820e2878bd6e4f4c576c4a9cc095a3ff4bf245
Themelih1 <melih.yilmaz904@gmail.com>
2025-06-20 15:46:24 +0200

    Update settings.py with Neon DATABASE_URL

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -111,10 +111,11 @@ CSP_STYLE_SRC = ["'self'", "'unsafe-inline'"]  # CSS için
 
 # DATABASE
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 # PostgreSQL için alternatif:
 # DATABASES = {
