7d76235f5630449269b0ba8e1e0a5eade29cdd25
Themelih1 <melih.yilmaz904@gmail.com>
2025-07-13 23:29:17 +0200

    removed compress

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ python manage.py migrate --noinput
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
-echo "Running compress..."
-python manage.py compress --force
 
 echo "Starting Gunicorn..."
 exec gunicorn mywebsite.wsgi:application --bind 0.0.0.0:${PORT:-8001}
